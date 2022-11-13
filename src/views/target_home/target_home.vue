5b42226042f865299bec6afdb0afdbcb5632d451
zhangxiang <1583526343@qq.com>
2022-11-13 17:55:51 +0800

    init

---
+++ src/views/target_home/target_home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const dateList = ref([
+  { startDate: '2022-11-14', endData: '2022-12-14' }
+])
+
+</script>
+
+<template>
+  <main>
+    <ul>
+      <li v-for="item of dateList" :key="item.startDate">
+        startDate: {{item.startDate}}
+        endData: {{item.endData}}
+      </li>
+    </ul>
+  </main>
+</template>
