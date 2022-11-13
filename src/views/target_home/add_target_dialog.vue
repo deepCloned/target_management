85e01b366f4811013eca8d707480cbc9741bd2d5
zhangxiang <1583526343@qq.com>
2022-11-13 21:28:31 +0800

    组件引入、页面初始化

---
+++ src/views/target_home/add_target_dialog.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+  showDialog: Boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  showDialog: false
+})
+
+</script>
+
+<template>
+  <el-dialog
+    v-model="false"
+    title="Tips"
+    width="30%"
+    :before-close="handleClose"
+  >
+    <span>This is a message</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
--- src/views/target_home/target_home.vue
+++ src/views/target_home/target_home.vue
@@ -1,18 +1,78 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-const dateList = ref([
-  { startDate: '2022-11-14', endData: '2022-12-14' }
+
+import { reactive, ref } from 'vue';
+import AddTargetDialog from './add_target_dialog.vue'
+const tableData = reactive([
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
 ])
+
+function handleEdit() {}
+function handleDelete() {}
+
+const showAddTarget = ref(false)
+function handleAdd() {
+  showAddTarget.value = true
+}
 
 </script>
 
 <template>
-  <main>
-    <ul>
-      <li v-for="item of dateList" :key="item.startDate">
-        startDate: {{item.startDate}}
-        endData: {{item.endData}}
-      </li>
-    </ul>
-  </main>
+  <section class="container">
+    <div class="header">
+      <el-button size="small" @click="handleAdd">Add Target</el-button>
+    </div>
+    <div class="table-data">
+      <el-table :data="tableData" style="width: 100%" border>
+        <el-table-column prop="date" label="Date" width="180" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address" />
+        <el-table-column label="Operations">
+          <template #default="scope">
+            <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
+              >Edit</el-button
+            >
+            <el-button
+              size="small"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+              >Delete</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <AddTargetDialog v-if="showAddTarget"></AddTargetDialog>
+  </section>
 </template>
+
+<style scoped>
+.container{
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.table-data{
+  margin-top: 20px;
+  width: 800px;
+}
+</style>
